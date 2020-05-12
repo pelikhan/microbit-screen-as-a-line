199ef9b252080485ef23a558494d4ae33f4381a7
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-05-12 09:08:21 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 namespace screenMagic {
+    /**
+     * Address LEDs linearly row first
+     */
+    //% blockId=screenmagicplotat
+    //% block="plot at index"
+    //% index.min=0 index.max=25
     export function plotAt(index: number): void {
         index |= 0
         const y = Math.floor(index / 5);
